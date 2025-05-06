e48b06f46b846b3f71438b444c01dc29de8e7cdb
Siuol7 <nguyencaotan03072000@gmail.com>
2025-05-06 14:46:22 +0300

    Completed ex00 CPP00

--- CPP-Module00/ex00/megaphone.cpp
+++ CPP-Module00/ex00/megaphone.cpp
@@ -6,22 +6,30 @@
 /*   By: caonguye <caonguye@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/21 03:37:58 by caonguye          #+#    #+#             */
-/*   Updated: 2025/05/06 14:30:57 by caonguye         ###   ########.fr       */
+/*   Updated: 2025/05/06 14:45:44 by caonguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <cstring>
 
 int	main(int ac, char **av)
 {
+	int len;
+
 	if (ac == 1)
 		std::cout << "* LOUD AND UNREADABLE FEEDBACK NOISE *" << std::endl;
 	else
 	{
 		for (int i = 1; i < ac; i++)
 		{
+			len = strlen(av[i]);
 			for (int j = 0; av[i][j]; j++)
+			{
+				if (av[i][j] == ' ' && (j == 0 || j == len - 1))
+					continue ;
 				std::cout << (char)std::toupper(av[i][j]);
+			}
 			if (i + 1 != ac)
 				std::cout << " ";
 		}
