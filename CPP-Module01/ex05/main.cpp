68965ad2b5a439f088a6603050281c37a5554cd8
Siuol7 <nguyencaotan03072000@gmail.com>
2025-05-07 11:07:39 +0300

    Update ex05 CPP01

--- CPP-Module01/ex05/main.cpp
+++ CPP-Module01/ex05/main.cpp
@@ -6,7 +6,21 @@
 /*   By: caonguye <caonguye@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 23:58:45 by caonguye          #+#    #+#             */
-/*   Updated: 2025/05/06 23:58:46 by caonguye         ###   ########.fr       */
+/*   Updated: 2025/05/07 11:04:21 by caonguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Harl.hpp"
+
+int	main(void)
+{
+	std::string	empty;
+	Harl		Harl;
+
+	Harl.complain(empty);
+	Harl.complain("DEBUG");
+	Harl.complain("INFO");
+	Harl.complain("WARNING");
+	Harl.complain("ERROR");
+	Harl.complain("DEBUGG");
+}
