8c290044f7ba1f7a33a5c539650967dffbcd1832
Siuol7 <nguyencaotan03072000@gmail.com>
2025-05-07 10:24:10 +0300

    Update ex05 CPP01

--- CPP-Module01/ex05/Harl.cpp
+++ CPP-Module01/ex05/Harl.cpp
@@ -6,8 +6,33 @@
 /*   By: caonguye <caonguye@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 23:58:53 by caonguye          #+#    #+#             */
-/*   Updated: 2025/05/06 23:59:01 by caonguye         ###   ########.fr       */
+/*   Updated: 2025/05/07 10:21:33 by caonguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
+
+void	Harl::_debug(void)
+{
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+}
+
+void	Harl::_info(void)
+{
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+}
+
+void	Harl::_warning(void)
+{
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+}
+
+void	Harl::_error(void)
+{
+	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+void	Harl::complain(std::string msg)
+{
+	
+}
--- CPP-Module01/ex05/Harl.hpp
+++ CPP-Module01/ex05/Harl.hpp
@@ -6,9 +6,22 @@
 /*   By: caonguye <caonguye@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 23:59:07 by caonguye          #+#    #+#             */
-/*   Updated: 2025/05/06 23:59:21 by caonguye         ###   ########.fr       */
+/*   Updated: 2025/05/07 09:57:12 by caonguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
 #include <iostream>
 #include <string>
+
+class Harl
+{
+	public:
+		void	complain(std::string level);
+	private:
+		void	_debug(void);
+		void	_info(void);
+		void	_warning(void);
+		void	_error(void);
+};
