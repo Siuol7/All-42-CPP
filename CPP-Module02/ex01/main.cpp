9e54be8dc11835d264d7dadf0e9c092bc55e8b29
Siuol7 <nguyencaotan03072000@gmail.com>
2025-05-14 17:41:48 +0300

    Update CPP2 ex01

--- CPP-Module02/ex01/main.cpp
+++ CPP-Module02/ex01/main.cpp
@@ -6,7 +6,28 @@
 /*   By: caonguye <caonguye@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 14:57:49 by caonguye          #+#    #+#             */
-/*   Updated: 2025/05/13 14:57:50 by caonguye         ###   ########.fr       */
+/*   Updated: 2025/05/14 17:41:43 by caonguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Fixed.hpp"
+
+int main( void )
+{
+	Fixed a;
+	Fixed const b(10);
+	Fixed const c(42.42f);
+	Fixed const d(b);
+	a = Fixed(1234.4321f);
+
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
+	std::cout << "c is " << c << std::endl;
+	std::cout << "d is " << d << std::endl;
+	
+	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+	return (0)
+}
