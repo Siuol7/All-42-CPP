294f3bafab4e2b4cb2e8e56dec7a54d09327d1be
Siuol7 <nguyencaotan03072000@gmail.com>
2025-05-14 17:44:06 +0300

    Update CPP2 ex01

--- CPP-Module02/ex01/main.cpp
+++ CPP-Module02/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: caonguye <caonguye@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 14:57:49 by caonguye          #+#    #+#             */
-/*   Updated: 2025/05/14 17:41:43 by caonguye         ###   ########.fr       */
+/*   Updated: 2025/05/14 17:42:30 by caonguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int main( void )
 {
-	Fixed a;
+	Fixed 		a;
 	Fixed const b(10);
 	Fixed const c(42.42f);
 	Fixed const d(b);
@@ -24,10 +24,10 @@ int main( void )
 	std::cout << "b is " << b << std::endl;
 	std::cout << "c is " << c << std::endl;
 	std::cout << "d is " << d << std::endl;
-	
+
 	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
 	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
 	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
 	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
-	return (0)
+	return (0);
 }
