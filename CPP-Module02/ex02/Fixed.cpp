5cfb4b6a64513ee4bfef34c5d3923e3be6805714
Siuol7 <nguyencaotan03072000@gmail.com>
2025-05-19 11:03:44 +0300

    Update ex02 CPP02

--- CPP-Module02/ex02/Fixed.cpp
+++ CPP-Module02/ex02/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: caonguye <caonguye@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/18 15:35:22 by caonguye          #+#    #+#             */
-/*   Updated: 2025/05/19 10:51:43 by caonguye         ###   ########.fr       */
+/*   Updated: 2025/05/19 11:02:18 by caonguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,7 @@ Fixed::Fixed(const int nb) : _value(nb << _frac_bits){};
 
 Fixed::Fixed(const float nb) : _value(roundf(nb * (1 << _frac_bits))){};
 
-Fixed::Fixed(const Fixed& other)
-{
-	*this = other;
-}
+Fixed::Fixed(const Fixed& other){*this = other;}
 
 Fixed& Fixed::operator= (const Fixed& other)
 {
@@ -36,7 +33,9 @@ int	Fixed::getRawBits(void) const {return _value;}
 
 void	Fixed::setRawBits(int const raw) {_value = raw;}
 
-float	toFloat
+float	Fixed::toFloat(void) const {return _value / (1 << _frac_bits);}
+
+int		Fixed::toInt(void) const {return _value >> _frac_bits;}
 
 bool	Fixed::operator>(const Fixed& other) const {return (this->_value > other._value) ? true : false;}
 
