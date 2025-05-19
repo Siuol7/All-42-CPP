4c91e722ae7088a16cf5d26b52360eb7561333d4
Siuol7 <nguyencaotan03072000@gmail.com>
2025-05-19 10:25:03 +0300

    Update ex02 CPP02

--- CPP-Module02/ex02/Fixed.hpp
+++ CPP-Module02/ex02/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: caonguye <caonguye@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/18 15:35:19 by caonguye          #+#    #+#             */
-/*   Updated: 2025/05/18 16:06:04 by caonguye         ###   ########.fr       */
+/*   Updated: 2025/05/19 10:24:05 by caonguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,5 +31,34 @@ class Fixed
 		float	toFloat(void) const;
 		int		toInt(void) const;
 
-		bool	
-}
+		//COMPARE
+		bool	operator>(const Fixed& other) const;
+		bool	operator<(const Fixed& other) const;
+		bool	operator>=(const Fixed& other) const;
+		bool	operator<=(const Fixed& other) const;
+		bool	operator!=(const Fixed& other) const;
+
+		//CALCULATING
+		Fixed	operator+(const Fixed& other) const;
+		Fixed	operator-(const Fixed& other) const;
+		Fixed	operator*(const Fixed& other) const;
+		Fixed	operator/(const Fixed& other) const;
+
+		//(IN/DE)CREMENT
+		Fixed& operator++(void);
+		Fixed& operator--(void);
+		Fixed	operator++(int);
+		Fixed	operator--(int);
+
+		//MAX/MIN
+		static	Fixed& 		max(Fixed& a, Fixed& b);
+		static	Fixed&		min(Fixed& a, Fixed& b);
+		static	const Fixed& 	max(const Fixed& a, const Fixed& b);
+		static	const Fixed&	min(const Fixed& a, const Fixed& b);
+
+	private:
+		int					_value;
+		static int const 	_frac_bits = 8;
+};
+
+std::ostream& operator<<(std::ostream &os, Fixed& obj);
