f5dcba53273ce23310d7b039e63ae8c31cab5119
Siuol7 <nguyencaotan03072000@gmail.com>
2025-05-19 11:34:54 +0300

    Update ex02 CPP02

--- CPP-Module02/ex02/Fixed.cpp
+++ CPP-Module02/ex02/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: caonguye <caonguye@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/18 15:35:22 by caonguye          #+#    #+#             */
-/*   Updated: 2025/05/19 11:02:18 by caonguye         ###   ########.fr       */
+/*   Updated: 2025/05/19 11:34:47 by caonguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,12 +37,55 @@ float	Fixed::toFloat(void) const {return _value / (1 << _frac_bits);}
 
 int		Fixed::toInt(void) const {return _value >> _frac_bits;}
 
-bool	Fixed::operator>(const Fixed& other) const {return (this->_value > other._value) ? true : false;}
+//COMPARE
 
-bool	Fixed::operator<(const Fixed& other) const {return (this->_value < other._value) ? true : false;}
+bool	Fixed::operator>(const Fixed& other) const {return (this->getRawBits() > other.getRawBits()) ? true : false;}
 
-bool	Fixed::operator>=(const Fixed& other) const {return (this->_value >= other._value) ? true : false;}
+bool	Fixed::operator<(const Fixed& other) const {return (this->getRawBits() < other.getRawBits()) ? true : false;}
 
-bool	Fixed::operator<=(const Fixed& other) const {return (this->_value <= other._value) ? true : false;}
+bool	Fixed::operator>=(const Fixed& other) const {return (this->getRawBits() >= other.getRawBits()) ? true : false;}
 
-bool	Fixed::operator!=(const Fixed& other) const {return (this->_value != other._value) ? true : false;}
+bool	Fixed::operator<=(const Fixed& other) const {return (this->getRawBits() <= other.getRawBits()) ? true : false;}
+
+bool	Fixed::operator!=(const Fixed& other) const {return (this->getRawBits() != other.getRawBits()) ? true : false;}
+
+//CALCULATING
+
+Fixed	Fixed::operator+(const Fixed& other) const {return Fixed(this->toFloat() + other.toFloat());}
+
+Fixed	Fixed::operator-(const Fixed& other) const {return Fixed(this->toFloat() - other.toFloat());}
+
+Fixed	Fixed::operator*(const Fixed& other) const {return Fixed(this->toFloat() * other.toFloat());}
+
+Fixed	Fixed::operator/(const Fixed& other) const {return Fixed(this->toFloat() / other.toFloat());}
+
+//(IN/DE)CREMENT
+
+Fixed&	Fixed::operator++(void)
+{
+	this->_value++;
+	return *this;
+}
+
+Fixed&	Fixed::operator--(void)
+{
+	this->_value--;
+	return (*this);
+}
+
+Fixed	Fixed::operator++(int none)
+{
+	Fixed	temp(*this);
+	this->_value++;
+	return (temp);
+}
+
+Fixed	Fixed::operator--(int none)
+{
+	Fixed	temp(*this);
+	this->_value--;
+	return (temp);
+}
+
+
+
