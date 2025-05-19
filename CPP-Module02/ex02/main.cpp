d1a5493f6bfb0661b9d79295472c435ab7562eca
Siuol7 <nguyencaotan03072000@gmail.com>
2025-05-19 11:57:57 +0300

    Update ex02 CPP02

--- CPP-Module02/ex02/main.cpp
+++ CPP-Module02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: caonguye <caonguye@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/18 15:35:16 by caonguye          #+#    #+#             */
-/*   Updated: 2025/05/19 11:44:03 by caonguye         ###   ########.fr       */
+/*   Updated: 2025/05/19 11:57:52 by caonguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,19 @@ int main( void )
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
 	std::cout << a << std::endl;
 	std::cout << ++a << std::endl;
+	std::cout << --a << std::endl;
 	std::cout << a << std::endl;
 	std::cout << a++ << std::endl;
 	std::cout << a << std::endl;
+	std::cout << a-- << std::endl;
+	std::cout << a << std::endl;
 	std::cout << b << std::endl;
+	std::cout << (a > b ? "yes" : "no" )<< std::endl;
+	std::cout << (a < b ? "yes" : "no" )<< std::endl;
+	std::cout << (a >= b ? "yes" : "no" )<< std::endl;
+	std::cout << (a <= b ? "yes" : "no" )<< std::endl;
+	std::cout << (a != b ? "yes" : "no" )<< std::endl;
 	std::cout << Fixed::max( a, b ) << std::endl;
+	std::cout << Fixed::min( a, b ) << std::endl;
 	return 0;
 }
