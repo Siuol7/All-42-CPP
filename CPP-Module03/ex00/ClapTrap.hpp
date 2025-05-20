87d56745dd1427dc93dd9aa3cf9771d5b5db52eb
Siuol7 <nguyencaotan03072000@gmail.com>
2025-05-20 16:10:26 +0300

    Update class

--- CPP-Module03/ex00/ClapTrap.hpp
+++ CPP-Module03/ex00/ClapTrap.hpp
@@ -6,9 +6,11 @@
 /*   By: caonguye <caonguye@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 14:24:03 by caonguye          #+#    #+#             */
-/*   Updated: 2025/05/20 14:39:17 by caonguye         ###   ########.fr       */
+/*   Updated: 2025/05/20 16:10:21 by caonguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#pragma once
 
 #include <iostream>
 #include <string>
@@ -20,11 +22,13 @@ class	ClapTrap
 		~ClapTrap();
 		ClapTrap(const ClapTrap& other);
 		ClapTrap&  operator=(const ClapTrap& other);
+
 		void	attack(const std::string& target);
 		void	takeDamage(unsigned int amount);
 		void	beRepaired(unsigned int amount);
 	private:
-		
-
-
-}
+		std::string		_name;
+		unsigned int	_hit_points;
+		unsigned int	_energy_points;
+		unsigned int	_attack_damage;
+};
