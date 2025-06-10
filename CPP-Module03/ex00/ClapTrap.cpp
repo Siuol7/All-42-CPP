2c536e8171db886533f6a9a77fb5217e283266ea
Siuol7 <nguyencaotan03072000@gmail.com>
2025-06-10 11:03:30 +0300

    Update CPP3 ex00

--- CPP-Module03/ex00/ClapTrap.cpp
+++ CPP-Module03/ex00/ClapTrap.cpp
@@ -6,9 +6,55 @@
 /*   By: caonguye <caonguye@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 16:09:54 by caonguye          #+#    #+#             */
-/*   Updated: 2025/05/20 16:10:15 by caonguye         ###   ########.fr       */
+/*   Updated: 2025/06/10 10:36:31 by caonguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 
+ClapTrap::ClapTrap() : _name("Undefined"),
+					_hit_points(),
+					_energy_points(),
+					_attack_damage()
+{
+	std::cout << "Default constructor called";
+}
+
+ClapTrap::~ClapTrap(){std::cout << "Default destructor called" << std::endl;}
+
+ClapTrap::ClapTrap(const ClapTrap& other)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = other;
+}
+
+ClapTrap& ClapTrap::operator= (const ClapTrap& other)
+{
+	std::cout << "Copy assignment called" << std::endl;
+	if (this != &other)
+	{
+		this->_name = other._name;
+		this->_hit_points = other._hit_points;
+		this->_energy_points = other._energy_points;
+		this->_attack_damage = other._attack_damage;
+	}
+	return *this;
+}
+
+void	ClapTrap::attack(const std::string& target)
+{
+	if (!this->_energy_points)
+		std::cout << "ClapTrap " << this->_name << "has no energy points to attack" << std::endl;
+	if (!this->_hit_points)
+		
+}
+
+void	ClapTrap::takeDamage(unsigned int amount)
+{
+
+}
+
+void	ClapTrap::beRepaired(unsigned int amount)
+{
+
+}
