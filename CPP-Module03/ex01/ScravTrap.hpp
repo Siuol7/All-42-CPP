0f8fe349567647621a915193a533d7ffc3b474ed
Siuol7 <nguyencaotan03072000@gmail.com>
2025-06-11 19:14:48 +0300

    Update all

--- CPP-Module03/ex01/ScravTrap.hpp
+++ CPP-Module03/ex01/ScravTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: caonguye <caonguye@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/11 16:19:20 by caonguye          #+#    #+#             */
-/*   Updated: 2025/06/11 19:10:42 by caonguye         ###   ########.fr       */
+/*   Updated: 2025/06/11 19:14:40 by caonguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,16 @@ class ScravTrap : public ClapTrap
 		ScravTrap();
 		ScravTrap(std::string name);
 		~ScravTrap() override;
-		Scrav
-}
+		ScravTrap(const ScravTrap& other);
+		ScravTrap& operator=(const ScravTrap& other);
+
+		void	attack(const std::string& target) override;
+		void	takeDamage(unsigned int amount);
+		void	beRepaired(unsigned int amount);
+
+	private:
+		std::string		_name;
+		unsigned int	_hit_points;
+		unsigned int	_energy_points;
+		unsigned int	_attack_damage;
+};
