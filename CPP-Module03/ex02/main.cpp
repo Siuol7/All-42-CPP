623510d63afa55ca14b1cbf7f12a92b598cf10ed
Siuol7 <nguyencaotan03072000@gmail.com>
2025-06-14 00:06:04 +0300

    Update testcases

--- CPP-Module03/ex02/main.cpp
+++ CPP-Module03/ex02/main.cpp
@@ -6,35 +6,22 @@
 /*   By: caonguye <caonguye@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/09 16:14:04 by caonguye          #+#    #+#             */
-/*   Updated: 2025/06/13 09:45:25 by caonguye         ###   ########.fr       */
+/*   Updated: 2025/06/14 00:01:18 by caonguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 #include "ScravTrap.hpp"
+#include "FragTrap.hpp"
 
 int	main(void)
 {
-	ScravTrap	st1;
-	ClapTrap	ct2("A");
-	ClapTrap	ct3("B");
-	ScravTrap	st2("C");
-	ScravTrap 	st3(st2);
+	ClapTrap	ct1;
+	ClapTrap	ct2("CT2");
+	ScravTrap	st1("ST1");
+	ScravTrap	st2(st1);
+	FragTrap	ft1("FT1");
 
-	std::cout << std::endl << "First match" << std::endl;
-
-	for (int i = 0; i < 10; i++)
-	{
-		st1.attack("A");
-		ct2.takeDamage(2);
-		ct2.beRepaired(1);
-	}
-
-	std::cout << std::endl << "Next match" << std::endl;
-	st1.attack("B");
-	ct3.takeDamage(2);
-	ct3.beRepaired(1);
-
-	std::cout << std::endl;
 	st2.guardGate();
+	ft1.highFivesGuys();
 }
