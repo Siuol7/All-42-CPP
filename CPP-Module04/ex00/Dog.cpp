3ac128a956a1c284cdd832729230b7560ef8ea05
Siuol7 <nguyencaotan03072000@gmail.com>
2025-06-16 17:23:57 +0300

    update Dog functions

--- CPP-Module04/ex00/Dog.cpp
+++ CPP-Module04/ex00/Dog.cpp
@@ -6,9 +6,32 @@
 /*   By: caonguye <caonguye@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 18:42:09 by caonguye          #+#    #+#             */
-/*   Updated: 2025/06/16 17:19:32 by caonguye         ###   ########.fr       */
+/*   Updated: 2025/06/16 17:23:20 by caonguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Dog.hpp"
 
+Dog::Dog()
+{
+	this->_type = "Dog";
+	std::cout << "Dog : Default constructor called" << std::endl;
+}
+
+Dog::~Dog(){std::cout << "Dog : Default destructor called" << std::endl;}
+
+Dog::Dog(const Dog& other) : Animal(other)
+{
+	this->_type = other._type;
+	std::cout << "Dog : Copy constructor called" << std::endl;
+}
+
+Dog& Dog::operator=(const Dog& other)
+{
+	Animal::operator=(other);
+	std::cout << "Dog : Copy assignment called" << std::endl;
+	return *this;
+}
+
+void	Dog::makeSound() const
+{std::cout << "Dog sounds" << std::endl;}
--- CPP-Module04/ex00/Dog.hpp
+++ CPP-Module04/ex00/Dog.hpp
@@ -6,7 +6,7 @@
 /*   By: caonguye <caonguye@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 18:42:17 by caonguye          #+#    #+#             */
-/*   Updated: 2025/06/16 17:22:13 by caonguye         ###   ########.fr       */
+/*   Updated: 2025/06/16 17:23:47 by caonguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 #include "Animal.hpp"
 
-class Dog
+class Dog : public Animal
 {
 	public:
 		Dog();
