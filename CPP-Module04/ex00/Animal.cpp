fbb3d1470559b7640bc743a90b187be345273755
Siuol7 <nguyencaotan03072000@gmail.com>
2025-06-16 16:24:43 +0300

    update Animal functions

--- CPP-Module04/ex00/Animal.cpp
+++ CPP-Module04/ex00/Animal.cpp
@@ -6,7 +6,7 @@
 /*   By: caonguye <caonguye@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 18:31:41 by caonguye          #+#    #+#             */
-/*   Updated: 2025/06/16 14:06:35 by caonguye         ###   ########.fr       */
+/*   Updated: 2025/06/16 16:22:27 by caonguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,3 +14,23 @@
 
 Animal::Animal() : _type("Undefined Animal")
 {std::cout << "Animal: Default constructor called" << std::endl;}
+
+Animal::~Animal(){std::cout << "Animal: Default destructor called" << std::endl;}
+
+Animal::Animal(const Animal& other)
+{
+	std::cout << "Animal : Copy constructor called" << std::endl;
+	*this = other;
+}
+
+Animal& Animal::operator=(const Animal& other)
+{
+	std::cout << "Animal : Copy assignment called" << std::endl;
+	if (this != &other)
+		this->_type = other._type;
+	return *this;
+}
+
+void	Animal::makeSound(void){std::cout << "Animal's sounds" << std::endl;}
+
+std::string	Animal::getType(void){return this->_type;}
