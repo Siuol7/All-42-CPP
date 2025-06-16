d3bafca2b9d3ed1adc2f2d7cc73bf6bfb7646b3d
Siuol7 <nguyencaotan03072000@gmail.com>
2025-06-16 14:45:13 +0300

    Update Animal class

--- CPP-Module04/ex00/Animal.cpp
+++ CPP-Module04/ex00/Animal.cpp
@@ -6,8 +6,11 @@
 /*   By: caonguye <caonguye@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 18:31:41 by caonguye          #+#    #+#             */
-/*   Updated: 2025/06/15 18:31:49 by caonguye         ###   ########.fr       */
+/*   Updated: 2025/06/16 14:06:35 by caonguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Animal.hpp"
+
+Animal::Animal() : _type("Undefined Animal")
+{std::cout << "Animal: Default constructor called" << std::endl;}
--- CPP-Module04/ex00/Animal.hpp
+++ CPP-Module04/ex00/Animal.hpp
@@ -6,7 +6,7 @@
 /*   By: caonguye <caonguye@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 18:31:52 by caonguye          #+#    #+#             */
-/*   Updated: 2025/06/15 18:39:18 by caonguye         ###   ########.fr       */
+/*   Updated: 2025/06/16 13:32:04 by caonguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,15 @@
 
 class	Animal
 {
+	public :
+		Animal();
+		~Animal();
+		Animal(const Animal& other);
+		Animal& operator=(const Animal& other);
 
+		virtual void makeSound();
+		std::string	getType();
+
+	protected:
+		std::string	_type;
 };
