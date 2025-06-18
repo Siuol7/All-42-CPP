25bf24f6a698412a66aa3d36a0c2a729234de8a0
Siuol <nguyencaotan03072000@gmail.com>
2025-06-18 09:53:05 +0300

    Update ex00 CPP4

--- CPP-Module04/ex00/WrongCat.cpp
+++ CPP-Module04/ex00/WrongCat.cpp
@@ -15,3 +15,20 @@
 WrongCat::WrongCat() : _type("WrongCat")
 {std::cout << "WrongCat : Default constructor called" << std::endl;}
 
+WrongCat::~WrongCat(){std::cout << "WrongCat : Default destructor called" << std::endl;}
+
+WrongCat::WrongCat(const WrongCat& other)
+{
+    std::cout << "WrongCat : Copy constructor called" << std::endl;
+    *this = other;
+}
+
+WrongCat& WrongCat::operator=(const WrongCat& other)
+{
+    std::cout << "WrongCat : Copy assignment called" << std::endl;
+    WrongAnimal::operator=(other);
+    return *this;
+}
+
+void    WrongCat::makeSound(void)
+{std::cout << "WrongCat sound" << std::endl;}
