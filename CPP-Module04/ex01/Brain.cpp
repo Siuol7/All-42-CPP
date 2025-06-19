76d75de5e389e54992a30d980f3a5fc8f8655c1f
Siuol <nguyencaotan03072000@gmail.com>
2025-06-19 13:00:31 +0300

    Update Brain

--- CPP-Module04/ex01/Brain.cpp
+++ CPP-Module04/ex01/Brain.cpp
@@ -6,10 +6,29 @@
 /*   By: siuol <siuol@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/18 10:29:23 by siuol             #+#    #+#             */
-/*   Updated: 2025/06/19 08:22:05 by siuol            ###   ########.fr       */
+/*   Updated: 2025/06/19 12:52:53 by siuol            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Brain.hpp"
 
-Brain::Brain()
+Brain::Brain(){std::cout << "Brain : Default constructor called" << std::endl;}
+
+Brain::~Brain(){std::cout << "Brain : Default destructor called" << std::endl;}
+
+Brain::Brain(const Brain& other)
+{
+    std::cout << "Brain : Copy constructor called" << std::endl;
+    *this = other;
+}
+
+Brain& Brain::operator=(const Brain& other)
+{
+    std::cout << "Brain : Copy assignment called" << std::endl;
+    if (this != &other)
+    {
+        for (int i = 0; i < 100; i++)
+            this->_ideas[i] = other._ideas[i];
+    }
+    return *this
+}
