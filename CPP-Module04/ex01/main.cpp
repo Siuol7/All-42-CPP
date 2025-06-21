f6fff7d74c408ef27b28be80153178df346fbf0a
Siuol <nguyencaotan03072000@gmail.com>
2025-06-21 12:32:39 +0300

    Upodate main.cpp

--- CPP-Module04/ex01/main.cpp
+++ CPP-Module04/ex01/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: caonguye <caonguye@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: siuol <siuol@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 19:51:37 by caonguye          #+#    #+#             */
-/*   Updated: 2025/06/16 19:33:35 by caonguye         ###   ########.fr       */
+/*   Updated: 2025/06/21 12:32:26 by siuol            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,3 @@
 #include "Cat.hpp"
 #include "Dog.hpp"
 
-static void	ft_del(Animal *target)
-{
-	delete target;
-}
-
-static void	ft_sound(Animal& target)
-{
-	target.makeSound();
-}
-
-int	main(void)
-{
-	Animal a = Animal();
-	Animal *c = new Cat();
-	Dog d;
-	Dog e(d);
-
-	ft_sound(d);
-	ft_sound(e);
-	c->makeSound();
-	ft_del(c);
-}
