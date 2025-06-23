79ae1d7c2b0c71240b10a530832876d1b0d7338d
Siuol <nguyencaotan03072000@gmail.com>
2025-06-24 01:01:21 +0300

    Update header files

--- CPP-Module04/ex03/AMateria.cpp
+++ CPP-Module04/ex03/AMateria.cpp
@@ -6,8 +6,14 @@
 /*   By: siuol <siuol@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/23 18:58:03 by siuol             #+#    #+#             */
-/*   Updated: 2025/06/23 18:58:18 by siuol            ###   ########.fr       */
+/*   Updated: 2025/06/24 01:01:12 by siuol            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "AMateria.hpp"
+
+AMateria::AMateria() : _type("Undefined")
+{std::cout << "AMateria : Default constructor called" << std::endl;}
+
+AMateria::AMateria(std::string const &type) : _type(type)
+{std::cout << "AMateria : Constructor called" << std::endl;}
