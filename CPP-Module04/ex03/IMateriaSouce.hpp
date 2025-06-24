448590a4658697c2e14003bd712a9f7d249e4a57
Siuol <nguyencaotan03072000@gmail.com>
2025-06-25 01:24:57 +0300

    Update Ice

--- CPP-Module04/ex03/IMateriaSouce.hpp
+++ CPP-Module04/ex03/IMateriaSouce.hpp
@@ -1,14 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   MateriaSource.hpp                                  :+:      :+:    :+:   */
+/*   IMateriaSouce.hpp                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: siuol <siuol@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/25 01:15:31 by siuol             #+#    #+#             */
-/*   Updated: 2025/06/25 01:15:36 by siuol            ###   ########.fr       */
+/*   Created: 2025/06/25 01:16:16 by siuol             #+#    #+#             */
+/*   Updated: 2025/06/25 01:24:40 by siuol            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
+#include <iostream>
+#include <string>
+#include "AMateria.hpp"
+
+class IMateriaSource
+{
+    public:
+    virtual ~IMateriaSource() {}
+    virtual void learnMateria(AMateria*) = 0;
+    virtual AMateria* createMateria(std::string const & type) = 0;
+};
