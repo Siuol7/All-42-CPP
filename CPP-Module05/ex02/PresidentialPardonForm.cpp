ea55c9fd00c70049dd55a2fc0ecf1befa1c3d3dc
Siuol <nguyencaotan03072000@gmail.com>
2025-07-01 01:44:11 +0300

    Update DiamondTrap CPP5 ex02

--- CPP-Module05/ex02/PresidentialPardonForm.cpp
+++ CPP-Module05/ex02/PresidentialPardonForm.cpp
@@ -6,13 +6,13 @@
 /*   By: siuol <siuol@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/30 00:43:06 by siuol             #+#    #+#             */
-/*   Updated: 2025/07/01 01:28:52 by siuol            ###   ########.fr       */
+/*   Updated: 2025/07/01 01:44:08 by siuol            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PresidentialPardonForm.hpp"
 
-PresidentialPardonForm::PresidentialPardonForm(std::string target) : AForm("PPF.default", 0, 145, 137),
+PresidentialPardonForm::PresidentialPardonForm(std::string target) : AForm("PPF.default", 0, 25, 5),
                                                                     _target(target)
 {LOG_GREEN("PresidentialPardonForm : Constructor called");}
 
--- CPP-Module05/ex02/RobotomyRequestForm.cpp
+++ CPP-Module05/ex02/RobotomyRequestForm.cpp
@@ -6,13 +6,13 @@
 /*   By: siuol <siuol@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/30 00:42:46 by siuol             #+#    #+#             */
-/*   Updated: 2025/07/01 01:28:12 by siuol            ###   ########.fr       */
+/*   Updated: 2025/07/01 01:43:53 by siuol            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "RobotomyRequestForm.hpp"
 
-RobotomyRequestForm::RobotomyRequestForm(std::string target) : AForm("RRF.default", 0, 145, 137),
+RobotomyRequestForm::RobotomyRequestForm(std::string target) : AForm("RRF.default", 0, 72, 45),
                                                                     _target(target)
 {LOG_GREEN("RobotomyRequestForm : Constructor called");}
 
