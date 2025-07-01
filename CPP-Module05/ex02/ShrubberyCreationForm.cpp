590c4d2a1e469c3df0efe6e4f51b8f352c001404
Siuol7 <nguyencaotan03072000@gmail.com>
2025-07-01 14:06:59 +0300

    Update ascii tree

---
+++ CPP-Module05/ex02/ShrubberyCreationForm.cpp
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ShrubberyCreationForm.cpp                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: caonguye <caonguye@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/30 00:42:24 by siuol             #+#    #+#             */
+/*   Updated: 2025/07/01 14:06:36 by caonguye         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ShrubberyCreationForm.hpp"
+
+ShrubberyCreationForm::ShrubberyCreationForm(std::string target) : AForm("SCF.default", 0, 145, 137),
+                                                                    _target(target)
+{LOG_GREEN("ShrubberyCreationForm : Constructor called");}
+
+ShrubberyCreationForm::~ShrubberyCreationForm(){LOG_GREEN("ShrubberyCreationForm : Default destructor called");}
+
+ShrubberyCreationForm::ShrubberyCreationForm(const ShrubberyCreationForm& other) : AForm(other),
+                                                                                    _target(other._target)
+{LOG_GREEN("ShrubberyCreationForm : Copy constructor called");}
+
+void    ShrubberyCreationForm::execute(const Bureaucrat& executor) const
+{
+	AForm::execute(executor);
+	std::ofstream	outf(this->_target + "_shrubbery");
+	if (!outf)
+		throw std::runtime_error("SCF : cannot create/open " + this->_target +"_shrubbery file.\n");
+	std::string	tree = R"( .              .              ;%     ;;
+                   ,           ,                :;%  %;
+                    :         ;                   :;%;'     .,
+           ,.        %;     %;            ;        %;'    ,;
+             ;       ;%;  %%;        ,     %;    ;%;    ,%'
+              %;       %;%;      ,  ;       %;  ;%;   ,%;'
+               ;%;      %;        ;%;        % ;%;  ,%;'
+                `%;.     ;%;     %;'         `;%%;.%;'
+                 `:;%.    ;%%. %@;        %; ;@%;%'
+                    `:%;.  :;bd%;          %;@%;'
+                      `@%:.  :;%.         ;@@%;'
+                        `@%.  `;@%.      ;@@%;
+                          `@%%. `@%%    ;@@%;
+                            ;@%. :@%%  %@@%;
+                              %@bd%%%bd%%:;
+                                #@%%%%%:;;
+                                %@@%%%::;
+                                %@@@%(o);  . '
+                                %@@@o%;:(.,'
+                            `.. %@@@o%::;
+                               `)@@@o%::;
+                                %@@(o)::;
+                               .%@@@@%::;
+                               ;%@@@@%::;.
+                              ;%@@@@%%:;;;.
+                          ...;%@@@@@%%:;;;;,..    Gilo97)";
+	outf << tree;
+	outf.close();
+}
+
