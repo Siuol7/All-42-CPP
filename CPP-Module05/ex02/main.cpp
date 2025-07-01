2fb3570fd88ceada1b5dc1da4423f618b26418dc
Siuol <nguyencaotan03072000@gmail.com>
2025-07-02 01:38:43 +0300

    Update main Form

--- CPP-Module05/ex02/main.cpp
+++ CPP-Module05/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: siuol <siuol@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/27 09:07:01 by siuol             #+#    #+#             */
-/*   Updated: 2025/07/02 00:13:27 by siuol            ###   ########.fr       */
+/*   Updated: 2025/07/02 01:38:21 by siuol            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,23 @@ int main(void)
 {
     try
     {
-        Bureaucrat                  A("A" , 1);
+        Bureaucrat                  A("A" , 30);
         AForm       *SCF1 = new ShrubberyCreationForm("SCF1");
+        AForm       *SCF2 = new ShrubberyCreationForm("SCF2");
+        AForm       *RRF1 = new RobotomyRequestForm("RRF1");
+        AForm       *RRF2 = new RobotomyRequestForm("RRF2");
+        AForm       *PPF1 = new PresidentialPardonForm("PPF1");
+        AForm       *PPF2 = new PresidentialPardonForm("PPF2");
         std::cout << A << std::endl;
         A.signForm(*SCF1);
         A.executeForm(*SCF1);
+        A.executeForm(*SCF2);
+        A.signForm(*RRF1);
+        A.executeForm(*RRF1);
+        A.executeForm(*RRF2);
+        A.signForm(*PPF1);
+        A.executeForm(*PPF1);
+        A.executeForm(*PPF2);
     }
     catch(const std::exception& e)
     {
