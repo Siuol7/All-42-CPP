7c608eb68ce3b3224798c9672cf954804da9b66c
Siuol <nguyencaotan03072000@gmail.com>
2025-07-03 00:12:33 +0300

    Update main.cpp

--- CPP-Module05/ex03/Intern.cpp
+++ CPP-Module05/ex03/Intern.cpp
@@ -6,7 +6,7 @@
 /*   By: siuol <siuol@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/02 02:22:14 by siuol             #+#    #+#             */
-/*   Updated: 2025/07/02 13:51:00 by siuol            ###   ########.fr       */
+/*   Updated: 2025/07/03 00:11:58 by siuol            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,18 @@ Intern::Intern(){LOG_GREEN("Intern : Default constructor called");}
 
 Intern::~Intern(){LOG_GREEN("Intern : Default destructor called");}
 
-Intern::Intern(const Intern& other){LOG_GREEN("Intern : Copy constructor called");}
+Intern::Intern(const Intern& other)
+{
+    LOG_GREEN("Intern : Copy constructor called");
+    (void)other;
+}
 
-Intern& Intern::operator=(const Intern& other){LOG_GREEN("Intern : Copy assignment called");}
+Intern& Intern::operator=(const Intern& other)
+{
+    LOG_GREEN("Intern : Copy assignment called");
+    (void)other;
+    return *this;
+}
 
 //Execution
 
@@ -53,7 +62,7 @@ AForm*  Intern::makeForm(const std::string& type, const std::string& target)
 {
     size_t  size = sizeof(this->_formType) / sizeof(this->_formType[0]);
 
-    for (int i = 0; i < size; i++)
+    for (size_t i = 0; i < size; i++)
     {
         if (this->_formType[i] == type)
             return this->_formMaker[i](target);
