6c430cb8861d98e900bc60945674396a001dd34d
Siuol <nguyencaotan03072000@gmail.com>
2025-07-03 00:13:53 +0300

    Update main display

--- CPP-Module05/ex03/main.cpp
+++ CPP-Module05/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: siuol <siuol@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/27 09:07:01 by siuol             #+#    #+#             */
-/*   Updated: 2025/07/02 23:53:19 by siuol            ###   ########.fr       */
+/*   Updated: 2025/07/03 00:13:16 by siuol            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,16 +29,21 @@ int main(void)
         AForm       *RRF2 = I.makeForm("RobotomyRequestForm", "RRF2");
         AForm       *PPF1 = I.makeForm("PresidentialPardonForm", "PPF1");
         AForm       *PPF2 = I.makeForm("PresidentialPardonForm", "PPF2");
+        std::cout << std::endl;
         std::cout << A << std::endl;
+        std::cout << std::endl;
         A.signForm(*SCF1);
         A.executeForm(*SCF1);
         A.executeForm(*SCF2);
+        std::cout << std::endl;
         A.signForm(*RRF1);
         A.executeForm(*RRF1);
         A.executeForm(*RRF2);
+        std::cout << std::endl;
         A.signForm(*PPF1);
         A.executeForm(*PPF1);
         A.executeForm(*PPF2);
+        std::cout << std::endl;
     }
     catch(const std::exception& e)
     {
