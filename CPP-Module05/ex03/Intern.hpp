6fb71ce4bf6bada56b7d531e2aeeebc66010ea5f
Siuol <nguyencaotan03072000@gmail.com>
2025-07-02 12:22:59 +0300

    Intern.cpp

--- CPP-Module05/ex03/Intern.hpp
+++ CPP-Module05/ex03/Intern.hpp
@@ -6,7 +6,7 @@
 /*   By: siuol <siuol@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/02 02:22:10 by siuol             #+#    #+#             */
-/*   Updated: 2025/07/02 03:52:57 by siuol            ###   ########.fr       */
+/*   Updated: 2025/07/02 12:11:53 by siuol            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 #include <iostream>
 #include <string>
+#include <functional>
+#include "AForm.hpp"
 
 #define LOG_GREEN(msg) std::cout << "\033[32m" << msg << "\033[0m" << std::endl
 #define LOG_RED(msg)   std::cerr << "\033[31m" << msg << "\033[0m" << std::endl
@@ -25,4 +27,10 @@ class Intern
         ~Intern();
         Intern(const Intern& other);
         Intern& operator=(const Intern& other);
-}
+
+        AForm*  makeForm(const std::string& type, const std::string& target);
+
+    private :
+        static const std::string    _formType[];
+        static std::function<AForm*(const std::string& type)> _formMaker[];
+};
