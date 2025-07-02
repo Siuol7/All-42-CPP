86001f14b055d81f5a8e80c75a3337c65a63a427
Siuol <nguyencaotan03072000@gmail.com>
2025-07-03 01:15:50 +0300

    Update ex00 cpp6 OCF

--- CPP-Module06/ScalarConverter.hpp
+++ CPP-Module06/ScalarConverter.hpp
@@ -6,7 +6,20 @@
 /*   By: siuol <siuol@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/03 01:11:48 by siuol             #+#    #+#             */
-/*   Updated: 2025/07/03 01:11:49 by siuol            ###   ########.fr       */
+/*   Updated: 2025/07/03 01:15:32 by siuol            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
+#include <iostream>
+#include <string>
+
+class ScalarConverter
+{
+    private :
+        ScalarConverter() = delete;
+        ~ScalarConverter() = delete;
+        ScalarConverter(const ScalarConverter& other) = delete;
+        ScalarConverter& operator=(const ScalarConverter& other) = delete;
+};
