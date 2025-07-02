4904edd5b24938011663f2429b90c614c806b7ec
Siuol <nguyencaotan03072000@gmail.com>
2025-07-02 13:11:35 +0300

    OCF Intern

--- CPP-Module05/ex03/Intern.cpp
+++ CPP-Module05/ex03/Intern.cpp
@@ -6,8 +6,25 @@
 /*   By: siuol <siuol@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/02 02:22:14 by siuol             #+#    #+#             */
-/*   Updated: 2025/07/02 03:51:27 by siuol            ###   ########.fr       */
+/*   Updated: 2025/07/02 13:11:24 by siuol            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Intern.hpp"
+
+//OCF
+
+Intern::Intern(){LOG_GREEN("Intern : Default constructor called");}
+
+Intern::~Intern(){LOG_GREEN("Intern : Default destructor called");}
+
+Intern::Intern(const Intern& other){LOG_GREEN("Intern : Copy constructor called");}
+
+Intern& Intern::operator=(const Intern& other){LOG_GREEN("Intern : Copy assignment called");}
+
+//Execution
+
+AForm*  makeForm(const std::string& type, const std::string& target)
+{
+    
+}
