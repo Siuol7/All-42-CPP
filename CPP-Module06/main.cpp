66294900138c4639add3e4a8a818fa6ec15948aa
Siuol <nguyencaotan03072000@gmail.com>
2025-07-06 23:22:17 +0300

    Update checker cpp char converter

--- CPP-Module06/main.cpp
+++ CPP-Module06/main.cpp
@@ -6,7 +6,19 @@
 /*   By: siuol <siuol@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/03 01:11:57 by siuol             #+#    #+#             */
-/*   Updated: 2025/07/03 01:11:58 by siuol            ###   ########.fr       */
+/*   Updated: 2025/07/06 23:21:26 by siuol            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ScalarConverter.hpp"
+
+int main(int ac, char **av)
+{
+    if (ac != 2)
+    {
+        std::cout << "Missing input!" << std::endl;
+        return (1);
+    }
+    ScalarConverter::convert(av[1]);
+    return (0);
+}
