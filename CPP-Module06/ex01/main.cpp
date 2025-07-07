8048884529e40199eb25f3b84865f76b4036f191
Siuol <nguyencaotan03072000@gmail.com>
2025-07-07 22:52:50 +0300

    Start ex02

--- CPP-Module06/ex01/main.cpp
+++ CPP-Module06/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: siuol <siuol@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 20:24:14 by siuol             #+#    #+#             */
-/*   Updated: 2025/07/07 21:01:18 by siuol            ###   ########.fr       */
+/*   Updated: 2025/07/07 21:34:15 by siuol            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,19 @@ int main()
     
     LOG_GREEN("--After Serialization--");
     Data*       deSerialized  = Serialization::deserialize(Serialized);
+    std::cout << "deSerialized       : " << deSerialized << std::endl;
+    std::cout << "deSerialized name  : " << deSerialized->name << std::endl;
+    std::cout << "deSerialized value : " << deSerialized->value << std::endl << std::endl;
+
+    ptr.name = "B";
+    ptr.value = 43;
+    LOG_GREEN("--Before Serialization--");
+    std::cout << "Original address   : " << &ptr << std::endl;
+    std::cout << "Original name      : " << ptr.name << std::endl;
+    std::cout << "Original value     : " << ptr.value << std::endl;
+    std::cout << "serialized         : " << Serialized << std::endl;
+    
+    LOG_GREEN("--After Serialization--");
     std::cout << "deSerialized       : " << deSerialized << std::endl;
     std::cout << "deSerialized name  : " << deSerialized->name << std::endl;
     std::cout << "deSerialized value : " << deSerialized->value << std::endl;
