150c93b79c12ba4e12b0786c9560f1be2209ddfe
Siuol <nguyencaotan03072000@gmail.com>
2025-07-08 01:07:43 +0300

    Update Base.cpp

--- CPP-Module06/ex02/main.cpp
+++ CPP-Module06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: siuol <siuol@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 22:52:34 by siuol             #+#    #+#             */
-/*   Updated: 2025/07/08 00:44:10 by siuol            ###   ########.fr       */
+/*   Updated: 2025/07/08 00:58:14 by siuol            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,21 @@
 
 int main(void)
 {
+    Base* b;
     try
     {
-        Base* b = generate();
-        LOG_GREEN("Pointer dynamic cast : ");
-        identify(b);
-        std::cout << std::endl;
-        LOG_GREEN("Reference dynamic cast : ");
-        identify(*b);
+        b = generate();
     }
     catch(std::exception& e)
     {
         LOG_RED("Bad alloc");
     }
+
+    LOG_GREEN("Pointer dynamic cast : ");
+    identify(b);
+    std::cout << std::endl;
+    LOG_GREEN("Reference dynamic cast : ");
+    identify(*b);
+
+    delete b;
 }
