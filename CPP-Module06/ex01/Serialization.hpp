4875224150450663a868c134df7978fb3b4c118c
Siuol <nguyencaotan03072000@gmail.com>
2025-07-07 19:47:17 +0300

    Update Serialization.hpp

--- CPP-Module06/ex01/Serialization.hpp
+++ CPP-Module06/ex01/Serialization.hpp
@@ -6,7 +6,7 @@
 /*   By: siuol <siuol@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 16:50:39 by siuol             #+#    #+#             */
-/*   Updated: 2025/07/07 16:51:16 by siuol            ###   ########.fr       */
+/*   Updated: 2025/07/07 19:17:41 by siuol            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,18 @@
 
 #include <iostream>
 #include <string>
+#include <cstdint>
+#include "Data.hpp"
 
 class   Serialization
 {
-    
+    public  :
+        uintptr_t   serialize(Data* ptr);
+        Data*       deserialize(uintptr_t raw);
+
+    private :
+        Serialization() = delete;
+        ~Serialization() = delete;
+        Serialization(const Serialization& other) = delete;
+        Serialization& operator=(const Serialization& other) = delete;
 };
