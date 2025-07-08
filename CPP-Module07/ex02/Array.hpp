5d064acadbb282272f504005b54dd83d29ceaebc
Siuol7 <nguyencaotan03072000@gmail.com>
2025-07-08 17:07:42 +0300

    Start ex02 CPP7 + makefile

---
+++ CPP-Module07/ex02/Array.hpp
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: caonguye <caonguye@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/08 17:06:00 by caonguye          #+#    #+#             */
+/*   Updated: 2025/07/08 17:07:23 by caonguye         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+#include <string>
+
+#define LOG_GREEN(msg) std::cout << "\033[32m" << msg << "\033[0m" << std::endl
+#define LOG_RED(msg)   std::cerr << "\033[31m" << msg << "\033[0m" << std::endl
