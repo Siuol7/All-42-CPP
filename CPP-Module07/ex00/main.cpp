c943a548768cebd8acacc785a5467fc4b256adab
Siuol7 <nguyencaotan03072000@gmail.com>
2025-07-08 14:11:30 +0300

    Update main

--- CPP-Module07/ex00/main.cpp
+++ CPP-Module07/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: caonguye <caonguye@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/08 01:17:41 by siuol             #+#    #+#             */
-/*   Updated: 2025/07/08 14:02:09 by caonguye         ###   ########.fr       */
+/*   Updated: 2025/07/08 14:11:05 by caonguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,42 @@ int main(void)
     int a = 1;
     int b = 2;
     swap(a, b);
-    std::cout << "a = " << a << std::endl << "b = " << b;   
+    std::cout << "a = " << a << std::endl << "b = " << b << std::endl;;
+    
+    std::cout << std::endl;
+    
+    LOG_GREEN("--SWAPPING STR--");
+    std::string a1 = "a";
+    std::string b1 = "b";
+    swap(a1, b1);
+    std::cout << "a1 = " << a1 << std::endl << "b1 = " << b1 << std::endl;;
+
+    std::cout << std::endl;
+    
+    LOG_GREEN("--MIN INT--");
+    int a2 = 1;
+    int b2 = 2;
+    std::cout << "min = " << min(a2, b2) << std::endl;;
+
+    std::cout << std::endl;
+    
+    LOG_GREEN("--MIN FLOAT--");
+    float a3 = 1.5;
+    float b3 = -2.3;
+    std::cout << "min = " << min(a3, b3) << std::endl;
+
+    std::cout << std::endl;
+    
+    LOG_GREEN("--MAX INT--");
+    int a4 = 1;
+    int b4 = 2;
+    std::cout << "max = " << max(a4, b4) << std::endl;;
+
+    std::cout << std::endl;
+    
+    LOG_GREEN("--MAX FLOAT--");
+    float a5 = 1.5;
+    float b5 = -2.3;
+    std::cout << "max = " << max(a5, b5);
+    
 }
