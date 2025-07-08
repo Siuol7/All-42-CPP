e61a95f71761c729bf1a559b256d54bc71aadd2a
Siuol7 <nguyencaotan03072000@gmail.com>
2025-07-08 13:56:40 +0300

    Update hpp

--- CPP-Module07/ex00/whatever.hpp
+++ CPP-Module07/ex00/whatever.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   whatever.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: siuol <siuol@student.42.fr>                +#+  +:+       +#+        */
+/*   By: caonguye <caonguye@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/08 01:17:32 by siuol             #+#    #+#             */
-/*   Updated: 2025/07/08 12:29:02 by siuol            ###   ########.fr       */
+/*   Updated: 2025/07/08 13:56:10 by caonguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,3 +17,24 @@
 
 #define LOG_GREEN(msg) std::cout << "\033[32m" << msg << "\033[0m" << std::endl
 #define LOG_RED(msg)   std::cerr << "\033[31m" << msg << "\033[0m" << std::endl
+
+template<typename T>
+void    swap(T& a, T& b)
+{
+    T   temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
+
+template<typename T>
+T   const min(const T& a, const T& b)
+{
+    return (a < b ? a : b);
+}
+
+template<typename T>
+T   const max(const T& a, const T& b)
+{
+    return (a > b ? a : b);
+}
