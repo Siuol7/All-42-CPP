faa88b31d3324c6f00c740c7207f20eefe29f008
Siuol <nguyencaotan03072000@gmail.com>
2025-07-08 12:29:13 +0300

    Start ex00

--- CPP-Module07/ex00/whatever.hpp
+++ CPP-Module07/ex00/whatever.hpp
@@ -6,7 +6,14 @@
 /*   By: siuol <siuol@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/08 01:17:32 by siuol             #+#    #+#             */
-/*   Updated: 2025/07/08 01:17:33 by siuol            ###   ########.fr       */
+/*   Updated: 2025/07/08 12:29:02 by siuol            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
+#include <iostream>
+#include <string>
+
+#define LOG_GREEN(msg) std::cout << "\033[32m" << msg << "\033[0m" << std::endl
+#define LOG_RED(msg)   std::cerr << "\033[31m" << msg << "\033[0m" << std::endl
