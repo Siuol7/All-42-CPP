68a9d95731b4731c7333c0d02db785df27f8e874
Siuol7 <nguyencaotan03072000@gmail.com>
2025-07-08 17:14:58 +0300

    Start ex02 CPP7 + makefile

--- CPP-Module07/ex02/Array.hpp
+++ CPP-Module07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: caonguye <caonguye@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/08 17:06:00 by caonguye          #+#    #+#             */
-/*   Updated: 2025/07/08 17:07:23 by caonguye         ###   ########.fr       */
+/*   Updated: 2025/07/08 17:07:49 by caonguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,3 +17,4 @@
 
 #define LOG_GREEN(msg) std::cout << "\033[32m" << msg << "\033[0m" << std::endl
 #define LOG_RED(msg)   std::cerr << "\033[31m" << msg << "\033[0m" << std::endl
+
