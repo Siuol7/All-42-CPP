8b603d902ed9136f94a425c23bb5cfbf93a84738
Siuol7 <nguyencaotan03072000@gmail.com>
2025-07-09 16:36:21 +0300

    Update ex01 CPP8

--- CPP-Module08/ex01/Span.hpp
+++ CPP-Module08/ex01/Span.hpp
@@ -6,11 +6,27 @@
 /*   By: caonguye <caonguye@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/09 14:30:32 by caonguye          #+#    #+#             */
-/*   Updated: 2025/07/09 14:32:17 by caonguye         ###   ########.fr       */
+/*   Updated: 2025/07/09 16:35:54 by caonguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#pragma once
 
 #include <iostream>
 #include <string>
 #include <algorithm>
 #include <vector>
+
+class Span
+{
+    public  :
+        Span() = default;
+        ~Span() = default;
+        Span(const Span& other) = default;
+        Span& operator=(const Span& other) = default;
+
+        void    addNumber(int n);
+        
+    private :
+        unsigned int    N;
+};
