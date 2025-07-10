ba8d3e61a4d1dd60318e2b8e2c29c858f3bd0806
Siuol <nguyencaotan03072000@gmail.com>
2025-07-10 23:54:35 +0300

    Update Span.hpp

--- CPP-Module08/ex01/Span.cpp
+++ CPP-Module08/ex01/Span.cpp
@@ -6,9 +6,25 @@
 /*   By: siuol <siuol@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/09 14:30:28 by caonguye          #+#    #+#             */
-/*   Updated: 2025/07/09 22:50:02 by siuol            ###   ########.fr       */
+/*   Updated: 2025/07/10 23:43:26 by siuol            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Span.hpp"
 
+void    Span::addNumber(int n)
+{
+    if (this->_storage.size() == this->_N)
+        throw std::runtime_error("No space left");
+    this->_storage.end() = n;
+}
+
+unsigned int    Span::shortestSpan()
+{
+    
+}
+
+unsigned int    Span::longestSpan()
+{
+    
+}
