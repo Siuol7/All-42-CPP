a801e31cfff61a9b821bc7684fa081a17fbb7585
Siuol <nguyencaotan03072000@gmail.com>
2025-07-11 03:10:24 +0300

    Update main.cpp

--- CPP-Module08/ex01/main.cpp
+++ CPP-Module08/ex01/main.cpp
@@ -3,11 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: caonguye <caonguye@student.42.fr>          +#+  +:+       +#+        */
+/*   By: siuol <siuol@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/09 14:29:56 by caonguye          #+#    #+#             */
-/*   Updated: 2025/07/09 14:30:03 by caonguye         ###   ########.fr       */
+/*   Updated: 2025/07/11 03:09:55 by siuol            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Span.hpp"
+
+int main(void)
+{
+    Span span;
+    
+    try
+    {
+        LOG_GREEN("--NORMAL CASE--");
+        span.addNumber(1);
+        span.addNumber(2);
+        span.addNumber(3);
+        std::cout << "Shortest span is: " << span.shortestSpan() << std::endl;
+        std::cout << "Longest span is : " << span.longestSpan() << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        LOG_RED(e.what());
+    }
+    
+}
