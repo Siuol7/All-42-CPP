a142b09413673126b2275aa8bac4ae919e742900
Siuol <nguyencaotan03072000@gmail.com>
2025-07-13 03:12:23 +0300

    Start CPP8 ex02 with OCF

--- CPP-Module08/ex02/MutantStack.hpp
+++ CPP-Module08/ex02/MutantStack.hpp
@@ -6,7 +6,22 @@
 /*   By: siuol <siuol@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/12 05:21:56 by siuol             #+#    #+#             */
-/*   Updated: 2025/07/12 05:21:57 by siuol            ###   ########.fr       */
+/*   Updated: 2025/07/13 03:12:13 by siuol            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+template<typename T>
+class   MutantStack : public std::stack<T>
+{
+    public :
+        MutantStack();
+        ~MutantStack();
+        MutantStack(const other& MutantStack);
+        MutantStack& operator=(const other& MutantStack);
+};
