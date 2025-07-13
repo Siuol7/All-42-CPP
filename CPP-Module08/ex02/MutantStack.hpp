a563629ee40224403e4812d6eb11346a4c119750
Siuol <nguyencaotan03072000@gmail.com>
2025-07-13 06:26:37 +0300

    Update iterator

--- CPP-Module08/ex02/MutantStack.hpp
+++ CPP-Module08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: siuol <siuol@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/12 05:21:56 by siuol             #+#    #+#             */
-/*   Updated: 2025/07/13 04:47:36 by siuol            ###   ########.fr       */
+/*   Updated: 2025/07/13 06:26:21 by siuol            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <stack>
 
 template<typename T>
 class   MutantStack : public std::stack<T>
@@ -29,4 +30,10 @@ class   MutantStack : public std::stack<T>
                 std::stack<T>::operator=(other);
             return *this;
         }
+
+        using iterator = std::stack<T>::container_type::iterator;
+        
+        using const_iterator =  std::stack<T>::container_type::const_iterator;
+        using riterator = std::stack<T>::container_type::reverse_iterator;
+        using const_riterator = std::stack<T>::container_type::const_reverse_iterator;
 };
