df49c4757b52fb01f50b8049ddb884d5bec60108
Siuol <nguyencaotan03072000@gmail.com>
2025-07-14 00:45:04 +0300

    Imple

--- CPP-Module08/ex02/MutantStack.hpp
+++ CPP-Module08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: siuol <siuol@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/12 05:21:58 by siuol             #+#    #+#             */
-/*   Updated: 2025/07/14 00:37:44 by siuol            ###   ########.fr       */
+/*   Updated: 2025/07/14 00:45:00 by siuol            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,18 +33,18 @@ class   MutantStack : public std::stack<T>
 
         using iterator = std::stack<T>::container_type::iterator;
         iterator    begin(){return this->c.begin();}
-        iterator    end(return this->c.begin(););
+        iterator    end(){return this->c.end();}
         
         using const_iterator =  std::stack<T>::container_type::const_iterator;
-        const_iterator  begin(return this->c.begin(););
-        const_iterator  end(return this->c.begin(););
-        
+        const_iterator  begin(){return this->c.begin();}
+        const_iterator  end(){return this->c.end();}
+    
         using riterator = std::stack<T>::container_type::reverse_iterator;
-        riterator   begin();
-        riterator   end();
+        riterator   begin(){return this->c.rbegin();}
+        riterator   end(){return this->c.rend();}
         
         using const_riterator = std::stack<T>::container_type::const_reverse_iterator;
-        const_riterator begin();
-        const_riterator end();
+        const_riterator begin(){return this->c.rbegin();}
+        const_riterator end(){return this->c.rend();}
 };
 
