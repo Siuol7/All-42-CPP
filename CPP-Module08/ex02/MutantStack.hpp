b16eb0eb6f65a9c2f1288484ebfd104eb11d6b08
Siuol <nguyencaotan03072000@gmail.com>
2025-07-14 02:27:33 +0300

    Completed CPP08

--- CPP-Module08/ex02/MutantStack.hpp
+++ CPP-Module08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: siuol <siuol@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/12 05:21:58 by siuol             #+#    #+#             */
-/*   Updated: 2025/07/14 00:45:00 by siuol            ###   ########.fr       */
+/*   Updated: 2025/07/14 02:21:00 by siuol            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,34 +17,37 @@
 #include <algorithm>
 #include <stack>
 
+#define LOG_GREEN(msg) std::cout << "\033[32m" << msg << "\033[0m" << std::endl
+#define LOG_RED(msg)   std::cerr << "\033[31m" << msg << "\033[0m" << std::endl
+
 template<typename T>
 class   MutantStack : public std::stack<T>
 {
     public :
         MutantStack() : std::stack<T>(){};
-        ~MutantStack() : ~std::stack<T>(){};
-        MutantStack(const other& MutantStack) : std::stack<T>(other){};
-        MutantStack& operator=(const other& MutantStack)
+        ~MutantStack() = default;
+        MutantStack(const MutantStack& other) : std::stack<T>(other){};
+        MutantStack& operator=(const MutantStack& other)
         {
-            if (this != other)
+            if (this != &other)
                 std::stack<T>::operator=(other);
             return *this;
         }
 
-        using iterator = std::stack<T>::container_type::iterator;
+        using iterator = typename std::stack<T>::container_type::iterator;
         iterator    begin(){return this->c.begin();}
         iterator    end(){return this->c.end();}
         
-        using const_iterator =  std::stack<T>::container_type::const_iterator;
-        const_iterator  begin(){return this->c.begin();}
-        const_iterator  end(){return this->c.end();}
+        using const_iterator = typename std::stack<T>::container_type::const_iterator;
+        const_iterator  begin() const {return this->c.begin();}
+        const_iterator  end() const {return this->c.end();}
     
-        using riterator = std::stack<T>::container_type::reverse_iterator;
-        riterator   begin(){return this->c.rbegin();}
-        riterator   end(){return this->c.rend();}
+        using riterator = typename std::stack<T>::container_type::reverse_iterator;
+        riterator   rbegin(){return this->c.rbegin();}
+        riterator   rend(){return this->c.rend();}
         
-        using const_riterator = std::stack<T>::container_type::const_reverse_iterator;
-        const_riterator begin(){return this->c.rbegin();}
-        const_riterator end(){return this->c.rend();}
+        using const_riterator = typename std::stack<T>::container_type::const_reverse_iterator;
+        const_riterator rbegin() const {return this->c.rbegin();}
+        const_riterator rend() const {return this->c.rend();}
 };
 
--- CPP-Module08/ex02/main.cpp
+++ CPP-Module08/ex02/main.cpp
@@ -6,7 +6,65 @@
 /*   By: siuol <siuol@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/12 05:21:51 by siuol             #+#    #+#             */
-/*   Updated: 2025/07/12 05:21:52 by siuol            ###   ########.fr       */
+/*   Updated: 2025/07/14 02:27:09 by siuol            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "MutantStack.hpp"
+
+int main(void)
+{
+    LOG_GREEN("--INT Mutant Stack--");
+    MutantStack<int> ms;
+
+    ms.push(1);
+    ms.push(2);
+    ms.push(3);
+    
+    LOG_GREEN("---NORMAL---");
+    auto bit = ms.begin();
+    auto eit = ms.end();
+    
+    *bit = 9;
+    *(eit - 1) = 10;
+    for (auto i = bit; i != eit; i++)
+        std::cout << "Value in mutant stack : " << *i << std::endl;
+
+    std::cout << std::endl;
+    LOG_GREEN("---REVERSE---");
+    auto rbit = ms.rbegin();
+    auto reit = ms.rend();
+    
+    *rbit = 9;
+    *(reit - 1) = 10;
+    for (auto i = bit; i != eit; i++)
+        std::cout << "Value in mutant stack : " << *i << std::endl;
+
+    std::cout << std::endl;
+        
+    LOG_GREEN("--STR Mutant Stack--");
+    MutantStack<std::string> mstr;
+
+    mstr.push("a");
+    mstr.push("b");
+    mstr.push("c");
+    
+    LOG_GREEN("---NORMAL---");
+    auto bitr = mstr.begin();
+    auto eitr = mstr.end();
+    
+    *bitr = "e";
+    *(eitr - 1) = "f";
+    for (auto i = bitr; i != eitr; i++)
+        std::cout << "Value in mutant stack : " << *i << std::endl;
+
+    std::cout << std::endl;
+    LOG_GREEN("---REVERSE---");
+    auto rbitr = mstr.rbegin();
+    auto reitr = mstr.rend();
+    
+    *rbitr = "e";
+    *(reitr - 1) = "f";
+    for (auto i = bitr; i != eitr; i++)
+        std::cout << "Value in mutant stack : " << *i << std::endl;
+}
