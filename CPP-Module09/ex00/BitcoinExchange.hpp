1bea423ad36b44a121776ac45a341bca0346eaa8
Siuol7 <nguyencaotan03072000@gmail.com>
2025-07-14 14:06:03 +0300

    Update OCF

--- CPP-Module09/ex00/BitcoinExchange.hpp
+++ CPP-Module09/ex00/BitcoinExchange.hpp
@@ -6,7 +6,26 @@
 /*   By: caonguye <caonguye@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/14 14:00:31 by caonguye          #+#    #+#             */
-/*   Updated: 2025/07/14 14:00:32 by caonguye         ###   ########.fr       */
+/*   Updated: 2025/07/14 14:05:53 by caonguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
+#include  <iostream>
+#include  <string>
+#include  <algorithm>
+
+#define LOG_GREEN(msg) std::cout << "\033[32m" << msg << "\033[0m" << std::endl
+#define LOG_RED(msg)   std::cerr << "\033[31m" << msg << "\033[0m" << std::endl
+
+
+class BitcoinExchange
+{
+    public  :
+        BitcoinExchange();
+        ~BitcoinExchange();
+        BitcoinExchange(const BitcoinExchange& other);
+        BitcoinExchange& operator=(const BitcoinExchange& other);
+        
+};
