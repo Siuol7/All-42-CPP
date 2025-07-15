fda1d514b8b24fd0c3f667983a83b6ff1f3bd844
Siuol7 <nguyencaotan03072000@gmail.com>
2025-07-15 14:53:22 +0300

    Update

--- CPP-Module09/ex00/BitcoinExchange.cpp
+++ CPP-Module09/ex00/BitcoinExchange.cpp
@@ -6,7 +6,16 @@
 /*   By: caonguye <caonguye@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/14 14:00:28 by caonguye          #+#    #+#             */
-/*   Updated: 2025/07/14 14:00:29 by caonguye         ###   ########.fr       */
+/*   Updated: 2025/07/15 14:53:10 by caonguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "BitcoinExchange.hpp"
+
+BitcoinExchange::BitcoinExchange(){};
+
+BitcoinExchange::~BitcoinExchange(){};
+
+BitcoinExchange::BitcoinExchange(const BitcoinExchange& other){};
+
+BitcoinExchange& BitcoinExchange::operator=(const BitcoinExchange& other){};
--- CPP-Module09/ex00/BitcoinExchange.hpp
+++ CPP-Module09/ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: caonguye <caonguye@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/14 14:00:31 by caonguye          #+#    #+#             */
-/*   Updated: 2025/07/14 18:16:28 by caonguye         ###   ########.fr       */
+/*   Updated: 2025/07/15 14:51:42 by caonguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 #include  <algorithm>
 #include  <ctime>
 #include  <fstream>
+#include  <map>
 
 #define LOG_GREEN(msg) std::cout << "\033[32m" << msg << "\033[0m" << std::endl
 #define LOG_RED(msg)   std::cerr << "\033[31m" << msg << "\033[0m" << std::endl
@@ -29,5 +30,10 @@ class BitcoinExchange
         ~BitcoinExchange();
         BitcoinExchange(const BitcoinExchange& other);
         BitcoinExchange& operator=(const BitcoinExchange& other);
+
+        void    mapSetup(std::string path, std::string deli);
         
+    private :
+        std::map<std::string, double> _data;
+        std::map<std::string, double> _input;
 };
