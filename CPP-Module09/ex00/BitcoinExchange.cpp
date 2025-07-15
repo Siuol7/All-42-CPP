9cf2217a814c76f3818492c9051227f4729f8a67
Siuol7 <nguyencaotan03072000@gmail.com>
2025-07-15 16:35:08 +0300

    update mapping

--- CPP-Module09/ex00/BitcoinExchange.cpp
+++ CPP-Module09/ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: caonguye <caonguye@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/14 14:00:28 by caonguye          #+#    #+#             */
-/*   Updated: 2025/07/15 14:56:07 by caonguye         ###   ########.fr       */
+/*   Updated: 2025/07/15 16:34:45 by caonguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,48 @@ BitcoinExchange::BitcoinExchange(const BitcoinExchange& other){};
 
 BitcoinExchange& BitcoinExchange::operator=(const BitcoinExchange& other){};
 
-void    BitcoinExchange::mapSetup(std::string file, std::string deli, std::map<std::string, double> map)
+void    BitcoinExchange::mapSetup(std::string file, std::string deli, std::map<std::string, double> map, bool data)
 {
-    
+    std::ifstream   inf;
+    std::string     line;
+    std::string     key;
+    double          value;
+    size_t          pos;
+
+    inf.open(file);
+    if (!inf)
+        throw std::runtime_error("Cannot open " + file);
+    getline(inf, line);
+    while (getline(inf, line))
+    {
+        if (line.empty())
+            continue ;
+        pos = line.find(deli);
+        if (pos == std::string::npos)
+        {
+            std::cerr << "Error : bad input => " << line << std::endl;
+            continue ;
+        }
+        key = line.substr(0, pos);
+        try
+        {
+            value = std::stod(line.substr(pos + 1));
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << "Error : invalid value (" + line.substr(pos + 1) + ")" << std::endl;
+            continue ;
+        }
+        if (value < 0)
+        {
+            std::cerr << "Error : not a possitive number." << std::endl;
+            continue ;
+        }
+        if (value > 1000 && !data)
+        {
+            std::cerr << "Error : too large a number." << std::endl;
+            continue ;
+        }
+        map[key] = value;
+    }
 }
