f9088a89d3fde8c789cc0c0c9bddea9851bef5ff
Siuol7 <nguyencaotan03072000@gmail.com>
2025-07-16 13:05:54 +0300

    Update RPN.hpp

--- CPP-Module09/ex01/RPN.hpp
+++ CPP-Module09/ex01/RPN.hpp
@@ -3,10 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   RPN.hpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: siuol <siuol@student.42.fr>                +#+  +:+       +#+        */
+/*   By: caonguye <caonguye@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/16 10:20:13 by siuol             #+#    #+#             */
-/*   Updated: 2025/07/16 10:20:14 by siuol            ###   ########.fr       */
+/*   Updated: 2025/07/16 13:05:33 by caonguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+#include <stack>
+#include <string>
+
+#define LOG_GREEN(msg) std::cout << "\033[32m" << msg << "\033[0m" << std::endl
+#define LOG_RED(msg)   std::cerr << "\033[31m" << msg << "\033[0m" << std::endl
+
+class RPN
+{
+    public :
+        RPN();
+        ~RPN();
+        RPN(const RPN& other);
+        RPN& operator=(const RPN& other);
+
+        void    calculateRPN(std::string exp);
+};
