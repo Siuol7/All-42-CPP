b73b579f56bedc7192f8ce31aeb8a07c4d224272
Siuol <nguyencaotan03072000@gmail.com>
2025-07-19 05:38:43 +0300

    Start cpp09 ex02 with Makefile OCF

---
+++ CPP-Module09/ex02/PmergeMe.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: siuol <siuol@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/19 05:34:10 by siuol             #+#    #+#             */
+/*   Updated: 2025/07/19 05:38:07 by siuol            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <vector>
+#include <list>
+
+#define LOG_GREEN(msg) std::cout << "\033[32m" << msg << "\033[0m" << std::endl
+#define LOG_RED(msg)   std::cerr << "\033[31m" << msg << "\033[0m" << std::endl
+
+class PmergeMe
+{
+    public :
+        PmergeMe();
+        ~PmergeMe();
+        PmergeMe(const PmergeMe& other);
+        PmergeMe& operator=(const PmergeMe& other);
+};
