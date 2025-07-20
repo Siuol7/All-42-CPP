5032c5370435121bb3e94ed894f6ba11df86733f
Siuol7 <nguyencaotan03072000@gmail.com>
2025-07-20 14:11:40 +0300

    Update checking input function

--- CPP-Module09/ex02/PMergeMe.cpp
+++ CPP-Module09/ex02/PMergeMe.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   PMergeMe.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: siuol <siuol@student.42.fr>                +#+  +:+       +#+        */
+/*   By: caonguye <caonguye@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/19 05:34:08 by siuol             #+#    #+#             */
-/*   Updated: 2025/07/20 01:10:42 by siuol            ###   ########.fr       */
+/*   Updated: 2025/07/20 14:11:09 by caonguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,17 @@ PmergeMe::PmergeMe(char **av)
         while (stream >> token)
         {
             auto value_ptr = std::from_chars(token.data(), token.data() + token.length(), value);
+            
+
+            if (value_ptr.ec == std::errc())
+                continue;
+            else if (value_ptr.ec == std::errc::invalid_argument)
+                throw std::runtime_error("Error : Not an integer");
+            else if (value_ptr.ec == std::errc::result_out_of_range)
+                throw std::runtime_error("Error : Out of range number");
+            else if (value < 0)
+                throw std::runtime_error("Error : Not a positive integer");
+                
         }
     }
 }
