e58f4a9169801f299f1125b9c8315c9243150e2b
Siuol <nguyencaotan03072000@gmail.com>
2025-09-22 14:00:18 +0300

    Done ex01

--- ExamPractice/ex01/Warlock.hpp
+++ ExamPractice/ex01/Warlock.hpp
@@ -1,16 +1,23 @@
+
 #include <iostream>
 #include <string>
 
-class Warlock
+#pragma once
+
+
+class   Warlock
 {
     public  :
-        Warlock(std::string name, std::string title);
+        Warlock(const std::string& name, const std::string& title);
         ~Warlock();
+        Warlock(const Warlock& other) = delete;
+        Warlock &operator=(const Warlock& other) = delete;
+        const std::string   &getName() const;
+        const std::string   &getTitle() const;
+        void    setTitle(const std::string& title);
+        void    introduce() const;
 
     private :
         std::string _name;
         std::string _title;
-        Warlock();
-        Warlock(Warlock& other);
-        Warlock &operator=(Warlock& other);
 };
